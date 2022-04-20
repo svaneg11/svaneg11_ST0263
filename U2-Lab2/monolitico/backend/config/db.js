694594c799b1dee5bb6b0bda80c48022139e3e73
Santiago Vanegas <svaneg11@gmail.com>
2022-04-20 03:02:58 +0000

    changes

--- U2-Lab2/monolitico/backend/config/db.js
+++ U2-Lab2/monolitico/backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import colors from 'colors';
 
 const connectDB = async() =>{
-    process.env['NODE_ENV'] = "production"  
-    process.env['URL_DB_CONNECTION'] = 'mongodb://localhost/bookstore-production'
+      
+    
     try{
         const conn = await mongoose.connect(process.env.URL_DB_CONNECTION,{
             useUnifiedTopology: true,
