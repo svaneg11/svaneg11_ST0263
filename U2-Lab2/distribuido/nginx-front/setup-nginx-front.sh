6daf4c5cfe003c6a8e07543939ca335082b0a0eb
Santiago Vanegas <svaneg11@gmail.com>
2022-04-21 06:18:05 -0500

    add script to setup the nginx frontend

--- U2-Lab2/distribuido/nginx-front/setup-nginx-front.sh
+++ U2-Lab2/distribuido/nginx-front/setup-nginx-front.sh
@@ -10,7 +10,7 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot --nginx certonly -d www.svaneg11-lab2-distribuido.tk -d svaneg11-lab2-distribuido.tk
 mkdir -p ssl
 sudo cp /etc/letsencrypt/live/www.svaneg11-lab2-distribuido.tk/* /home/svaneg11/svaneg11_ST0263/U2-Lab2/distribuido/nginx-front/ssl
-kill -9 $(lsof -t -i:8080)
+kill -9 $(lsof -t -i:80)
 sudo chmod 777 ./../get-docker.sh
 ./../get-docker.sh
 docker-compose up --build
